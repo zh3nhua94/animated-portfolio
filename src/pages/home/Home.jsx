2ac7eefc607988d48d301db713d9a8f27e159c59
zh3nhua94 <zh3nhua94@gmail.com>
2023-11-12 00:55:29 +0800

    test scroll behavior

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-// import Navbar from "./components/navbar/Navbar";
 import Hero from "../../components/hero/Hero";
 import Parallax from "../../components/parallax/Parallax";
 import Services from "../../components/Services/Services";
 import Portfolio from "../../components/portfolio/Portfolio";
 import Contact from "../../components/contact/Contact";
-import ReactFullpage from "@fullpage/react-fullpage";
 
 const Home = () => {
 	return (
